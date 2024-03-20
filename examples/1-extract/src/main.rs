2fa6a1ad83f1d2e3882af2ee3b135d984133c84a
Alfredo Deza <alfredo@deza.pe>
2024-03-19 20:10:39 -0400

    add extract code examples

---
+++ examples/1-extract/src/main.rs
@@ -0,0 +1,77 @@
+use polars::prelude::*;
+use std::env;
+
+use std::fs::File;
+
+fn load(path: &str) -> PolarsResult<DataFrame> {
+    CsvReader::from_path(path)?.has_header(true).finish()
+}
+
+fn process(df: &DataFrame) {
+    println!("{:?}", df);
+    println!("{:?}", df.schema());
+    // Print data types of columns
+    println!("Data Types:");
+    for col in df.get_columns() {
+        println!("{:?}: {:?}", col.name(), col.dtype());
+    }
+    let df = drop_empty_columns(df.clone()).unwrap();
+    let new_df = replace_newlines(df).unwrap();
+    println!("{:?}", new_df);
+
+}
+
+fn drop_empty_columns(df: DataFrame) -> PolarsResult<DataFrame> {
+    let mut cols_to_drop = Vec::new();
+
+    for col_name in df.get_column_names() {
+        let col = df.column(&col_name).unwrap();
+
+        // Check if the column is not entirely null (not empty)
+        if col.null_count() == col.len() {
+            cols_to_drop.push(col_name);
+            println!("Dropping column: {}", col_name);
+        }
+    }
+
+    let new_df = df
+        .clone()
+        .lazy()
+        .select([col("*").exclude(cols_to_drop)])
+        .collect()?;
+
+    Ok(new_df)
+}
+
+
+fn replace_newlines(df: DataFrame) -> PolarsResult<DataFrame> {
+    
+    let new_df = df
+        .clone()
+        .lazy()
+        .with_columns([
+            col("notes").str()
+                .replace_all(lit(r"\\n|\\r"), lit(" "), false)
+                .alias("tmp_notes"),
+        ])
+        .select([col("*").exclude(&["notes"])])
+        .rename(&["tmp_notes"], &["notes"])
+        .collect()?;
+
+    Ok(new_df)
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <path to csv>", args[0]);
+        std::process::exit(1);
+    }
+
+    let csv_path = &args[1];
+    match load(csv_path) {
+        Ok(df) => process(&df),
+        Err(e) => eprintln!("{}", e),
+    }
+}
