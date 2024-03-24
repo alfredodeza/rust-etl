6277c049c37833acc37bc85c068317049966b63a
Alfredo Deza <alfredo@deza.pe>
2024-03-24 09:48:57 -0400

    add parquet saving on load example

--- examples/4-load/src/main.rs
+++ examples/4-load/src/main.rs
@@ -1,3 +1,25 @@
+use polars::prelude::*;
+use std::env;
+
+fn load(path: &str) -> PolarsResult<DataFrame> {
+    CsvReader::from_path(path)?.has_header(true).finish()
+}
+
+fn process(df: &mut DataFrame) {
+    let mut file = std::fs::File::create("wine-ratings.parquet").unwrap();
+    ParquetWriter::new(&mut file).finish(df).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <path to csv>", args[0]);
+        std::process::exit(1);
+    }
+
+    let csv_path = &args[1];
+    match load(csv_path) {
+        Ok(mut df) => process(&mut df),
+        Err(e) => eprintln!("{}", e),
+    }
 }
